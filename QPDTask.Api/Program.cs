2e258b8d7a91eaae37237b3662012c1ea6d17e96
oleyon <77335143+oleyon@users.noreply.github.com>
2023-11-20 03:07:42 +0300

    Add controllers support

--- QPDTask.Api/Program.cs
+++ QPDTask.Api/Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+
+
+services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var env = app.Environment;
+
+if (env.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+app.UseRouting();
+
+app.MapControllers();
 
 app.Run();
